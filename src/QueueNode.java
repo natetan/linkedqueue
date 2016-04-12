9103d784c6d18b8261a1c77429a03c7e0b3fd912
yulongtan <swifties@uw.edu>
2016-04-12 14:06:13 -0700

    added reference to previous node

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -5,13 +5,15 @@
 public class QueueNode<E> {
     public E data;
     public QueueNode next;
+    public QueueNode prev;
 
     public QueueNode(E data) {
-        this(data, null);
+        this(data, null, null);
     }
 
-    public QueueNode(E data, QueueNode next) {
+    public QueueNode(E data, QueueNode next, QueueNode prev) {
         this.data = data;
         this.next = next;
+        this.prev = prev;
     }
 }
