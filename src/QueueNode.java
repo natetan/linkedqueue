e35a1f616a93c7b479f3f479e719278a49524688
yulongtan <swifties@uw.edu>
2016-04-14 12:10:08 -0700

    added comments

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -1,16 +1,22 @@
 /**
- * Created by Yulong on 4/8/2016.
+ *  Yulong Tan
+ *  4.8.16
+ *
+ *  Node class for LinkedQueue
  */
 
 public class QueueNode<E> {
-    public E data;
-    public QueueNode next;
-    public QueueNode prev;
+    public E data; // Generic data for storage
+    public QueueNode next; // reference to the next node
+    public QueueNode prev; // reference to the previous node
 
+    // Constructs a QueueNode with given data, and null for
+    // its next node, and prev node.
     public QueueNode(E data) {
         this(data, null, null);
     }
 
+    // Constructs a QueueNode with given data, next node, and prev node
     public QueueNode(E data, QueueNode next, QueueNode prev) {
         this.data = data;
         this.next = next;
